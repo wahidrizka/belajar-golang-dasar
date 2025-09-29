d8f4634a3de68762f6483dd0b79a953e7e4d81e7
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-09-29 16:55:40 +0700

    add(comment): kode program comment

---
+++ comment.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+ * multiline comment
+ * multiline comment
+ * multiline comment
+ * multiline comment
+ * multiline comment
+ * multiline comment
+ */
+func main() {
+	// This is single line comment
+}
