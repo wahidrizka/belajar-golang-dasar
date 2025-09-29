df771d461b487dbf09a3a6fc45a464f236a6207e
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-09-29 17:35:03 +0700

    add(interface): kode program interface

---
+++ interface.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(value HasName) {
+	fmt.Println("Hello", value.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	person := Person{Name: "Wahid"}
+	SayHello(person)
+
+	animal := Animal{Name: "Kucing"}
+	SayHello(animal)
+
+}
